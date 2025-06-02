ad937f43807e64542386060a3d58b4c91a85f672
tranconghoc <hoctran381@gmail.com>
2025-06-02 22:56:50 +0700

    hoc test push branch

--- fashionstore-vue/src/components/Product.vue
+++ fashionstore-vue/src/components/Product.vue
@@ -523,12 +523,4 @@ body {
 .footer-list li {
   margin-bottom: 0.5rem; /* 8px */
 }
-html,
-body {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  overflow-x: hidden; /* Ngăn cuộn ngang không mong muốn */
-}
 </style>

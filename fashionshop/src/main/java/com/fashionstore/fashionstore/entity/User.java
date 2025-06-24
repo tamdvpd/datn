49a27a5601263b677508dfd1e784aa81db670ce2
Sỹ PD09903 <81215246+devunbelievable@users.noreply.github.com>
2025-06-24 20:40:59 +0700

    update Entity Order

--- fashionshop/src/main/java/com/fashionstore/fashionstore/entity/User.java
+++ fashionshop/src/main/java/com/fashionstore/fashionstore/entity/User.java
@@ -64,4 +64,7 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Cart> carts = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders = new ArrayList<>();
+
 }

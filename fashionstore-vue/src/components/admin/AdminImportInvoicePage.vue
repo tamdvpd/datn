5a3661583fc43f28bf57b360eed61b16ca2d49ee
Nguyenthihoaingoc <ngocnthpd10580@gmail.com>
2025-07-03 13:30:55 +0700

    hoàn thiện phiếu nhập hàng

--- fashionstore-vue/src/components/admin/AdminImportInvoicePage.vue
+++ fashionstore-vue/src/components/admin/AdminImportInvoicePage.vue
@@ -120,7 +120,7 @@ export default {
     },
     async fetchSuppliers() {
       try {
-        const res = await fetch('http://localhost:8080/api/suppliers');
+        const res = await fetch('http://localhost:8080/api/suppliers/active');git add .
         const data = await res.json();
         this.suppliers = data;
       } catch (err) {

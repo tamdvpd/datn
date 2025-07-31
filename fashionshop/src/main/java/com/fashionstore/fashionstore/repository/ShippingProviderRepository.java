a72395aa4d53338ac8ed0b662abd8fd4fbc776ee
Sỹ PD09903 <81215246+devunbelievable@users.noreply.github.com>
2025-07-31 17:26:19 +0700

    Cập nhật API backend để lấy các đơn vị vận chuyển còn đang hoạt đông(status=true)

--- fashionshop/src/main/java/com/fashionstore/fashionstore/repository/ShippingProviderRepository.java
+++ fashionshop/src/main/java/com/fashionstore/fashionstore/repository/ShippingProviderRepository.java
@@ -1,5 +1,6 @@
 package com.fashionstore.fashionstore.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import com.fashionstore.fashionstore.entity.ShippingProvider;
 
 public interface ShippingProviderRepository extends JpaRepository<ShippingProvider, Integer> {
     Optional<ShippingProvider> findByCode(String code);
+
+    List<ShippingProvider> findByStatusTrue();
 }
